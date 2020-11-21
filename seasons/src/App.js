886c03355c9b13aa7eee65e2577653ebee848d08
s-munro <68410363+s-munro@users.noreply.github.com>
2020-11-21 12:18:24 -0500

    basic scaffolding

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
   return (
     <div>
       <h2>Hey there!</h2>
