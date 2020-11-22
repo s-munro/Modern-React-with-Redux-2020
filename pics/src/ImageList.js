a3ed1d11e083fca4d104e29596e9f37ffaf70b8d
s-munro <68410363+s-munro@users.noreply.github.com>
2020-11-21 19:31:18 -0500

    added pics app

---
+++ pics/src/ImageList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ImageList extends React.Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default ImageList;
